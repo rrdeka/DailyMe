654f2c9aa525fde7a575b1c323cfcfdec2d7de3d
rrdeka <raktimnewdeka@gmail.com>
2025-08-14 18:44:59 +0530

    added cookies

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,43 @@ import ProtectedRoute from './components/ProtectedRoute'
 import Navbar from './components/Navbar'
 
 function App() {
+
+  useEffect(() => {
+    const setCookie = (name, value) => {
+      document.cookie = `${name}=${value}; path=/`
+    }
+
+    // analytics
+    setCookie('_ga', 'GA1.2.123456789')
+    setCookie('analytics_session', 'abc123')
+    setCookie('gtm_id', 'GTM-ABC123')
+
+    // marketing
+    setCookie('_fbp', 'fb.1.123456789')
+    setCookie('marketing_pixel', 'pixel123')
+    setCookie('ads_tracking', 'ads123')
+
+    // functional
+    setCookie('user_pref', 'dark_theme')
+    setCookie('lang', 'en')
+    setCookie('theme', 'dark')
+
+    // performance
+    setCookie('perf_tracking', 'perf123')
+    setCookie('speed_metric', 'speed123')
+    setCookie('load_time', 'load123')
+
+    // other
+    setCookie('random_cookie', 'random123')
+    setCookie('unknown_tracking', 'unknown123')
+    setCookie('misc_data', 'misc123')
+
+    // necessary
+    setCookie('session_id', 'sess123')
+    setCookie('csrf_token', 'token123')
+    setCookie('auth_token', 'auth123')
+  }, [])
+
   return (
     <AuthProvider>
       <div id="app" className="h-screen flex flex-col bg-gray-100">
